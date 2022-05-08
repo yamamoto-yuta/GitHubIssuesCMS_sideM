724175db0b6cdee327469cab44ae0353331e199d
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2022-05-08 11:27:42 +0900

    ✨ dl avatar&favocon

--- models/profile.py
+++ models/profile.py
@@ -1,10 +1,12 @@
 import os
 import json
 from urllib.parse import urlparse
+import urllib.request
 
 import yaml
+from PIL import Image
 
-from const import CONSTS_DIR 
+from const import CONSTS_DIR, PUBLIC_DIR, IMAGES_DIR
 
 class Profile():
     """ Profile dict of used in blog.
@@ -62,9 +64,63 @@ class Profile():
         self.profile['url_domain'] = parsed.netloc
         self.profile['url_subpath'] = parsed.path
 
+    def _dl_img(self, url):
+        try:
+            with urllib.request.urlopen(url) as web_file:
+                img = io.BytesIO(web_file.read())
+                img = Image.open(img)
+            return img
+        except urllib.error.URLError as e:
+            print(e)
+
+    def _format_img(self, img):
+        img = self._crop_img(img)
+        img = self._resize_img(img)
+        return img
+
+    def _crop_img(self, img):
+        """ 1:1になるようにクロップする """
+        w, h = img.width, img.height
+        crop_w = w
+        crop_h = w
+        if h < crop_h:
+            crop_w = h
+            crop_h = h
+        return img.crop(((w - crop_w) // 2,
+                         (h - crop_h) // 2,
+                         (w + crop_w) // 2,
+                         (h + crop_h) // 2))
+
+    def _resize_img(self, img):
+        resize_w = 64
+        resize_h = 64
+        w, h = img.width, img.height
+        if resize_w > w or resize_h > h:
+            resize_w = w
+            resize_h = h
+        img = img.resize((resize_w, resize_h))
+        return img
+
+    def save_img(self, img, save_dir, file_name, img_format, sizes=None):
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        if sizes is not None:
+            img.save(f'{save_dir}/{file_name}.{img_format}', sizes=sizes)
+        else:
+            img.save(f'{save_dir}/{file_name}.{img_format}')
+
+    def dl_img(self, url):
+        img = self._dl_img(url)
+        img = self._format_img(img)
+
     def save(self):
         if not os.path.exists(CONSTS_DIR):
             os.makedirs(CONSTS_DIR)
         with open(f'{CONSTS_DIR}/profile.json', 'w', encoding="utf-8") as f:
             json.dump(self.profile, f, indent=4, ensure_ascii=False)
+
+        favicon_img = self.dl_img(self.profile['favicon_url'])
+        self.save_img(favicon_img, PUBLIC_DIR+'/static', 'favicon', 'ico', sizes=[(16,16), (32, 32), (48, 48), (64,64)])
+        avatar_img = self.dl_img(self.profile['avatar_url'])
+        self.save_img(avatar_img, IMAGES_DIR+'/avatar', 'avatar', 'webp')
 
--- templates/profile.md
+++ templates/profile.md
@@ -7,17 +7,21 @@ assignees: ''
 
 ---
 ```yaml
+# Site settins
 blog_title: ENTER_TITLE_of_YOUR_BLOG
+root_url: ROOT_URL_OF_YOUR_FRONTEND_PAGE(index page)
+issues_edit_page: ISSUES_PAGE_of_YOUR_sideM (e.g. https://github.com/ShotaroKataoka/GitHubIssuesCMS_sideM/issues)
+favicon_url: IMAGE_URL. You can drag&drop here. Resize&Convert format automatically.
+avatar_url: IMAGE_URL. You can drag&drop here. Resize&Convert format automatically.
+
+# Profile settings
 author_name: ENTER_YOUR_NICK_NAME
 author_description: ENTER_YOUR_SHORT_INTRODUCTION
 copylight_name: ENTER_YOUR_NAME (for copyright component)
 copylight_url: ENTER_YOUR_URL (for copyright component)
-root_url: ROOT_URL_OF_YOUR_FRONTEND_PAGE(index page)
-issues_edit_page: ISSUES_PAGE_of_YOUR_sideM (e.g. https://github.com/ShotaroKataoka/GitHubIssuesCMS_sideM/issues)
 
-# Please rewrite your SNS url
-# This is an ordered SNS list
-# Unnecessary items can be deleted (or commented out)
+# Ordered your SNS list
+# Unnecessary items can be deleted (or commented out) and can be reordered.
 sns:
   - name: GitHub
     url: https://
